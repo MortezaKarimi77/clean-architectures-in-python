99aed9cd2bc348813088a089c360f82331ac2982
Morteza Karimi <itismortezakarimi@gmail.com>
2025-07-18 22:59:54 +0330

    feat(domain): auto-generate slug in Brand model

--- application/domain/brand.py
+++ application/domain/brand.py
@@ -1,8 +1,9 @@
-from typing import Annotated
+from typing import Annotated, Any
 
 from pydantic import Field
 
 from application.types import Slug
+from application.utils import slugify
 
 from .base import Base
 
@@ -11,5 +12,9 @@ class Brand(Base):
     id: int = Field(ge=1)
     name: str = Field(max_length=20)
     name_en: str = Field(max_length=20)
-    slug: Annotated[Slug, Field(max_length=20)]
+    slug: Annotated[Slug, Field(max_length=20)] = ""
     description: str = Field(max_length=5000)
+
+    def model_post_init(self, context: Any) -> None:
+        if not self.slug:
+            self.slug = slugify(self.name_en)
