bfa2f27af410b7901b9a61d63ef0960e1700bd72
Morteza Karimi <itismortezakarimi@gmail.com>
2025-07-18 21:53:39 +0330

    feat(domain): use Slug type for slug field in Brand model

--- application/domain/brand.py
+++ application/domain/brand.py
@@ -1,4 +1,8 @@
+from typing import Annotated
+
 from pydantic import Field
+
+from application.types import Slug
 
 from .base import Base
 
@@ -7,5 +11,5 @@ class Brand(Base):
     id: int = Field(ge=1)
     name: str = Field(max_length=20)
     name_en: str = Field(max_length=20)
-    slug: str = Field(max_length=20)
+    slug: Annotated[Slug, Field(max_length=20)]
     description: str = Field(max_length=5000)
